1c097bc9a34f13f750f52b84f14b5c91ce214fdb
dgmarie <0001dgmarie@gmail.com>
2022-12-28 14:39:14 -0500

    Fix some issues

--- gtk/src/install-css.sh
+++ gtk/src/install-css.sh
@@ -1,6 +1,3 @@
 #! /usr/bin/env bash
 mkdir -p "${DESTDIR}/${3}"
 cp -f "${1}" "${DESTDIR}/${2}"
-if [[ ! -z "${4}" ]] ; then
-  cp -f "${1}" "${DESTDIR}/${4}"
-fi
--- gtk/src/symlink.sh
+++ gtk/src/symlink.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
 mkdir -p "${DESTDIR}/${2}"
 
-ln -rsvf "${DESTDIR}/${1}/${3}" "${DESTDIR}/${2}"
+if [[ ! -z "${4}" ]]; then
+  ln -rsvf "${DESTDIR}/${1}/${3}" "${DESTDIR}/${2}/${4}"
+else
+  ln -rsvf "${DESTDIR}/${1}/${3}" "${DESTDIR}/${2}"
+fi
+
+if [[ ! -z "${5}" ]]; then
+  ln -rsvf "${DESTDIR}/${1}/${3}" "${DESTDIR}/${2}/${5}"
+fi
